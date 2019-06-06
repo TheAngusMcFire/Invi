4707a9d24209703b3f2cdd02c29e5b7963a321be
TheAngusMcFire <crieger1@gmx.at>
2019-06-06 23:46:30 +0200

    added dependencies and some testcode

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,19 @@
+
+struct SomeStruct
+{
+    test1 : u16,
+    test2 : u64,
+    sting : String
+}
+
 fn main() 
 {
-    println!("Hello, world!");
+    let tst = SomeStruct {test1 : 25, test2 : 23, sting : String::from( "lol test")};
+
+    test("this is a test", &tst);
+}
+
+fn test(msg : &str, stru : &SomeStruct)
+{
+    println!("Hello, world! {} {} {} {}",msg, stru.test1, stru.test2, stru.sting );
 }
