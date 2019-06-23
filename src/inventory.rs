1bcda95bd8addb7b33eb97b07a77f9f1f9ab61e7
TheAngusMcFire <crieger1@gmx.at>
2019-06-23 20:26:04 +0200

    added search prototype

--- src/inventory.rs
+++ src/inventory.rs
@@ -52,6 +52,25 @@ pub struct Tag
     id    : id_type
 }
 
+pub struct SearchResult <'a>
+{
+    compartments    : Vec<&'a Compartment>,
+    containers      : Vec<&'a Container>,
+    tags            : Vec<&'a Tag>,
+    items           : Vec<&'a Item>,
+}
+
+pub fn search<'a>(key_word : &str, inv :&'a Inventory) -> SearchResult <'a>
+{
+    return SearchResult 
+    {
+        compartments    : vec!(&inv.compartments[0]),
+        containers      : Vec::new(),
+        tags            : Vec::new(),
+        items           : Vec::new()
+    }
+}
+
 pub fn save_inventory(inventory : &Inventory, file_name : String) -> Result<(), Box<dyn Error>> 
 {
     let serialized = serde_json::to_string(&inventory)?;
@@ -74,10 +93,10 @@ pub fn new_inventory(file_name : String) -> Result<(), Box<dyn Error>>
         containers      : Vec::new(),
         tags            : Vec::new(),
         items           : Vec::new(),
-        cnt_compartment : 1,
-        cnt_container   : 1,
-        cnt_item        : 1,
-        cnt_tag         : 0,
+        cnt_compartment : 1 as id_type,
+        cnt_container   : 1 as id_type,
+        cnt_item        : 1 as id_type,
+        cnt_tag         : 0 as id_type,
     };
 
     let serialized = serde_json::to_string(&new_inventory)?;
